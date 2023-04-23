adcfc5a7883d634e298a8d3379b98667f595dfbc
Md. Mehedi Hasan Nabil <mehedihasannabil49@gmail.com>
2023-04-23 13:20:55 +0600

    done

--- js/main.js
+++ js/main.js
@@ -1,7 +1,14 @@
-const searchInput = document.getElementById('search')
 
-searchInput.addEventListener("change", function() {
-    console.log(this)
-})
+window.addEventListener("DOMContentLoaded", () => {
+    const searchInput = document.getElementById('search')
 
-console.log(searchInput)
+    searchInput.focus()
+
+    searchInput.addEventListener("keypress", (e) => {
+        const searchValue = searchInput.value
+        if (e.key === "Enter") {
+            window.open(`https://www.google.com/search?q=${searchValue}`)
+            searchInput.value = ""
+        }
+    });
+});
